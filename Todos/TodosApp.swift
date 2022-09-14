9022f8622128cb246d4adf051dd3f6f59cabf170
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-09-14 08:34:24 +0200

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Zaid Neurothrone on 2022-09-14.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
